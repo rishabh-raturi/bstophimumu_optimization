a802511b65dfc50cbaaa0d00d445e191fc12c701
Rishabh Raturi <rr26@iitbbs.ac.in>
2020-08-17 12:52:06 +0530

    Enternal const. fixed

--- Jpsi_MCFit/gauss_const.C
+++ Jpsi_MCFit/gauss_const.C
@@ -18,7 +18,7 @@ void gauss_const()
     	using namespace RooFit;
 
     	TChain *ch = new TChain("tree");
-	ch->Add("/home/rishabh/project/root_files/val/2016/red_ntuples/JPsi_MC_ForFit.root");
+	ch->Add("/home/rishabh/project/root_files/val/2016/red_ntuples/PhiMuMu_MC_ForFit.root");
     	TTree *tr = ch;
 
 	int nentries_ = tr->GetEntries();
@@ -38,10 +38,9 @@ void gauss_const()
     	RooRealVar Bcosalphabs2d("Bcosalphabs2d","Bcosalphabs2d", 0., 1.);
     	RooRealVar Bvtxcl("Bvtxcl","B vtx cl.", 0., 1.);
     	RooRealVar Triggers("Triggers","",0.,10.);
-	RooRealVar Q2("Q2", "", 1., 20.);
 
 	RooArgSet  observables(Bmass,Mumumass,Kmpt,Kppt,Kmtrkdcasigbs,Kptrkdcasigbs,Blxysig,Bcosalphabs2d,Bvtxcl);
-	observables.add(RooArgSet(Phimass,Triggers,Q2));
+	observables.add(RooArgSet(Phimass,Triggers));
 
 	RooDataSet data("data","dataset with Bmass", ch, observables);
 
@@ -51,12 +50,12 @@ void gauss_const()
     	RooDataSet *redData = (RooDataSet*)data.reduce(cutTotal);   //redData consists data to be fitted
 	
 	RooRealVar  mean("mean","common means for Crystal Balls", 5.367, bm_min, bm_max);
-	RooRealVar  sigma1("sigma1","sigma of CB1",  0.02, 0., 0.027);
-	RooRealVar  sigma2("sigma2","sigma of CB2",  0.04, 0., 0.049);
-	RooRealVar  sigM_frac("sigM_frac","fraction of CB", 0.5, 0.4, 0.7);
-	RooRealVar  n1("n1", "", 1., 140.);
-	RooRealVar  n2("n2", "", 1., 100.);
-	RooRealVar  alpha1("alpha1","alpha for CB1", 0.1, 4.);
+	RooRealVar  sigma1("sigma1","sigma of CB1",  0.04, 0., 1.);
+	RooRealVar  sigma2("sigma2","sigma of CB2",  0.027, 0., 1.);
+	RooRealVar  sigM_frac("sigM_frac","fraction of CB", 0., 1.);
+	RooRealVar  n1("n1", "", 1., 180.);
+	RooRealVar  n2("n2", "", 1., 180.);
+	RooRealVar  alpha1("alpha1","alpha for CB1", 0.1, 5.);
 	RooRealVar  alpha2("alpha2","alpha for CB2", -0.1, -5.0, 0.);
 
 	RooCBShape CB1("CB1","Crystal Ball-1", Bmass, mean, sigma1, alpha1,n1);
@@ -128,6 +127,7 @@ void gauss_const()
   	paveText->AddText(Form("frac = %.7f #pm %.7f ", sigM_frac.getVal(), sigM_frac.getError()));
 	paveText->AddText(Form("#alpha_{1} = %.7f #pm %.7f ", alpha1.getVal(), alpha1.getError()));
 	paveText->AddText(Form("#alpha_{2} = %.7f #pm %.7f ", alpha2.getVal(), alpha2.getError()));	
+	paveText->AddText(Form("n_{1} = %.7f #pm %.7f", n1.getVal(), n1.getError()));
         paveText->AddText(Form("n_{2} = %.7f #pm %.7f", n2.getVal(), n2.getError()));
   	paveText->AddText(Form("#chi^{2}/dof  = %.5f ", chi2dof ));
 
@@ -205,109 +205,10 @@ void gauss_const()
 	l1->Draw();
         l2->Draw();
         l3->Draw();
-	
-	TChain *ch4 =new TChain("tree");
-	ch4->Add("/home/rishabh/project/root_files/val/2016/red_ntuples/JKstar_MC_ForFit.root");
-	TTree *tr4 = ch4;
-
-	RooDataSet MC("MC","dataset with Bmass", ch4, observables);
-	RooDataSet *redMC = (RooDataSet*)MC.reduce(cutTotal);
-
-	RooRealVar  md("mean","common means for Crystal Balls", 5.278, bm_min, bm_max);
-        RooRealVar  sig1("sigma1","sigma of CB1",  0.04, 0., 1.);
-        RooRealVar  sig2("sigma2","sigma of CB2",  0.02, 0., 1.);
-        RooRealVar  MC_frac("sigM_frac","fraction of CB",  0., 1.);
-        RooRealVar  n1_mc("n1", "", 1., 140.);
-        RooRealVar  n2_mc("n2", "", 1., 140.);
-        RooRealVar  alp1("alpha1","alpha for CB1", 0.1, 5.);
-        RooRealVar  alp2("alpha2","alpha for CB2", -0.1, -5.0, 0.);
-
-	RooCBShape CB1_mc("CB1_mc","Crystal Ball-1", Bmass, md, sig1, alp1,n1_mc);
-        RooCBShape CB2_mc("CB2_mc","Crystal Ball-2", Bmass, md, sig2, alp2,n2_mc);
-
-	RooAddPdf CB_mc("CB_mc","CB1_mc+CB2_mc", RooArgList(CB1_mc,CB2_mc), RooArgList(MC_frac));
-        RooRealVar nsig_mc("nsig","nsig",1E3,500,1.1*tr4->GetEntries());
-        RooExtendPdf model_mc("model_mc","model", CB_mc, nsig_mc);
-
-	model_mc.Print("t");
-
-	TCanvas *c4 = new TCanvas("c4","c4",1500,1500);
-        TPad *k1   = new TPad("p1","p1", 0.1, 0.25, 0.995, 0.97);
-        TPad *k2   = new TPad("p2","p2", 0.1, 0.02, 0.995, 0.24);
-        k1->Draw();
-        k2->Draw();
-
-	k1->cd();
-	RooPlot *xframe_mc = Bmass.frame(Title(""), Bins(200));
-        xframe_mc->SetTitle("");
-        xframe_mc->GetXaxis()->SetTitle("#bf{m(K^{+}K^{-}#mu^{+}#mu^{-}) [GeV]}");
-
-	redMC->plotOn(xframe_mc,RooFit::Name("mc"));
-	model_mc.plotOn(xframe_mc, RooFit::Name("MC PDF"), LineColor(kBlue));
-	RooHist* hpull_mc = xframe->pullHist();
-
-	printf("**************************************");
-        printf("\n #sigma_{1}: %.7f", sig1.getVal());
-        printf("\n #sigma_{2}: %.7f", sig2.getVal());
-        double eff_sigma_mc = sqrt(MC_frac.getVal()*pow(sig1.getVal(),2) + (1 - MC_frac.getVal()) * pow(sig2.getVal(),2));
-        printf("\n #sigma_{eff}: %.7f", eff_sigma_mc);
-        printf("\n************************************\n");
-
-        double chi2dof_mc = xframe_mc->chiSquare();
-        std::cout<<"\n"<<std::endl;
-        std::cout<<"#chi^{2}/dof= "<< chi2dof_mc << std::endl;
-
-	model_mc.plotOn(xframe_mc, RooFit::Name("CB1_mc"), Components("CB1_mc"), LineStyle(kDashed), LineColor(kRed));
-	model_mc.plotOn(xframe_mc, RooFit::Name("CB2_mc"), Components("CB2_mc"), LineStyle(kDashed), LineColor(kGreen));
-
-	xframe_mc->Draw();
-
-	TPaveText* pT_mc = new TPaveText(0.65,0.60,0.83,0.88,"NDC");
-        pT_mc->SetBorderSize(0.0);
-        pT_mc->SetFillColor(kWhite);
-        pT_mc->SetFillStyle(0);
-        pT_mc->SetTextSize(0.02);
-        pT_mc->AddText(Form("Sig. Yield = %.0f #pm %.0f", nsig_mc.getVal(), nsig_mc.getError())); 
-        pT_mc->AddText(Form("#mu  = %.7f #pm %.7f GeV" , md.getVal() , md.getError()));
-        pT_mc->AddText(Form("#sigma_{1} = %.7f #pm %.7f GeV", sig1.getVal(), sig1.getError()));
-        pT_mc->AddText(Form("#sigma_{2} = %.7f #pm %.7f GeV", sig2.getVal(), sig2.getError()));
-        pT_mc->AddText(Form("#sigma_{eff} = %.7f  GeV", eff_sigma));
-        pT_mc->AddText(Form("frac = %.7f #pm %.7f ", MC_frac.getVal(), MC_frac.getError()));
-
-	pT_mc->AddText(Form("#alpha_{1} = %.7f #pm %.7f ", alp1.getVal(), alp1.getError()));
-	pT_mc->AddText(Form("#alpha_{2} = %.7f #pm %.7f ", alp2.getVal(), alp2.getError()));
-	pT_mc->AddText(Form("n_{1} = %.7f #pm %.7f", n1_mc.getVal(), n1_mc.getError()));
-	pT_mc->AddText(Form("n_{2} = %.7f #pm %.7f", n2_mc.getVal(), n2_mc.getError()));
-
-	pT_mc->Draw();
-
-	mark->Draw();
-
-	k2->cd();
-	
-	RooPlot* pul = Bmass.frame(Title(""));
-	pul->addPlotable(hpull_mc, "BX");
-	pul->SetMinimum(-5);
-    	pul->SetMaximum(5);
-        hpull_mc->SetFillColor(kBlue);
-    	pul->GetYaxis()->SetNdivisions(5);
-    	pul->SetTitle("#bf{Pull Distribution}");
-        pul->GetXaxis()->SetTitle("#bf{m(K^{+}K^{-}#mu^{+}#mu^{-}) [GeV]}");
-        pul->GetYaxis()->SetTitle("");
-        pul->GetYaxis()->SetLabelSize( (0.7/0.3)*xframe->GetXaxis()->GetLabelSize() );
-        pul->GetXaxis()->SetLabelSize( (0.7/0.3)*xframe->GetXaxis()->GetLabelSize() );
-   	pul->GetXaxis()->SetTitle("");
-    	pul->GetXaxis()->SetLimits(bm_min, bm_max);
-    	pul->Draw("P");
-
-	l1->Draw();
-	l2->Draw();
-	l3->Draw();
-
 
 	// using gaussian constrained parameters from MC to fit the data
 	TChain *ch1 = new TChain("tree");
-	ch1->Add("/home/rishabh/project/root_files/val/2016/red_ntuples/JPsi_Data_ForFit.root");
+	ch1->Add("/home/rishabh/project/root_files/val/2016/red_ntuples/PhiMuMu_Data_ForFit.root");
 	TTree *tr1 = ch1;
 	RooDataSet Data("Data","data sample 2016", ch1, observables);
 	RooDataSet *RedData = (RooDataSet*)Data.reduce(cutTotal);
@@ -317,19 +218,10 @@ void gauss_const()
         RooExponential bkgE("bkgE","exponential PDF", Bmass, lambda);
 	RooRealVar nbkgE("nbkgE","number of exponential bkg events", 8000, 0, 1E7);
 
-	RooRealVar nbkg_mc("nbkg_mc", "k star yield in data", 5000, 0, 1E4);
-
-	RooExtendPdf CBE("","", CB, nsigD);
-	
-	RooAddPdf PB("PB", "CB1_mc+CB2_mc", RooArgList(CB1_mc, CB2_mc), RooArgList(MC_frac));
-	RooExtendPdf PBE("PBE","PBE", PB, nbkg_mc);	
-	
-	RooAddPdf PCB("PCB", "PB+CB", RooArgList(CBE, PBE) );
 	//create final model for fitting
-        RooAddPdf Model("Model", "CB+e", RooArgList(PCB,bkgE), RooArgList(nsigD,nbkgE));
+        RooAddPdf Model("Model", "CB+e", RooArgList(CB,bkgE), RooArgList(nsigD,nbkgE));
         Model.Print("t");
 
-	//signal shape double crystal ball
 	RooGaussian   gaus_mean("gaus_mean", "gaus_mean", mean, RooConst(mean.getVal()), RooConst(mean.getError()));
 	RooGaussian   gaus_sigma1("gaus_sigma1", "gaus_sigma1", sigma1, RooConst(sigma1.getVal()), RooConst(sigma1.getError()));
 	RooGaussian   gaus_sigma2("gaus_sigma2", "gaus_sigma2", sigma2, RooConst(sigma2.getVal()), RooConst(sigma2.getError()));
@@ -339,20 +231,9 @@ void gauss_const()
 	RooGaussian   gaus_n2("gaus_n2", "gaus_n2", n2, RooConst(n2.getVal()), RooConst(n2.getError()));
 	RooGaussian   gaus_frac("gaus_frac", "gaus_frac", sigM_frac, RooConst(sigM_frac.getVal()), RooConst(sigM_frac.getError())); 
 
-	RooGaussian  g_npb("g_npb", "g_npb", nsig_mc, RooConst(nsig_mc.getVal()*0.78728), RooConst(nsig_mc.getError()*0.78728));
-	RooGaussian  g_sig1("g_sig1", "g_sig1", sig1, RooConst(sig1.getVal()), RooConst(sig1.getError()));
-	RooGaussian  g_sig2("g_sig2", "g_sig2", sig2, RooConst(sig2.getVal()), RooConst(sig2.getError()));
-        RooGaussian  g_alp1("g_alp1", "g_alp1", alp1, RooConst(alp1.getVal()), RooConst(alp1.getError()));
-        RooGaussian  g_alp2("g_alp2", "g_alp2", alp2, RooConst(alp2.getVal()), RooConst(alp2.getError()));
-        RooGaussian  g_n1_mc("g_n1", "g_n1", n1_mc, RooConst(n1_mc.getVal()), RooConst(n1_mc.getError()));
-        RooGaussian  g_n2_mc("g_n2", "g_n2", n2_mc, RooConst(n2_mc.getVal()), RooConst(n2_mc.getError()));
-        RooGaussian  g_frac("g_frac", "g_frac", MC_frac, RooConst(MC_frac.getVal()), RooConst(MC_frac.getError()));
-	
-
 	RooArgSet gausConstraints(gaus_sigma1, gaus_alpha1, gaus_n1, gaus_sigma2, gaus_alpha2, gaus_n2, gaus_frac);
-	RooArgSet gConstraints(g_sig1, g_sig2, g_alp1, g_alp2, g_n1_mc, g_n2_mc, g_frac, nbkg_mc );
 	
-	RooFitResult* fitres1 = Model.fitTo(*RedData, Extended(kTRUE), Save(kTRUE), Minimizer("Minuit"), Minos(kTRUE), ExternalConstraints(gausConstraints), Constrain(gConstraints));
+	RooFitResult* fitres1 = Model.fitTo(*RedData, Extended(kTRUE), Save(kTRUE), Minimizer("Minuit"), Minos(kTRUE), ExternalConstraints(gausConstraints));
 
 	TCanvas *c3 = new TCanvas("c3", "c3", 1250, 1500);
 	TPad *pad1   = new TPad("pad1","pad1", 0.01, 0.25, 0.995, 0.97);
@@ -482,13 +363,13 @@ void gauss_const()
         L2->Draw();
         L3->Draw();
 
-	c->SaveAs("Jpsi_MCFit_2016_Log.pdf");
-	c->SaveAs("Jpsi_MCFit_2016_Log.png");
+	c->SaveAs("rare_MCFit_2016_Log_q2.pdf");
+	c->SaveAs("rare_MCFit_2016_Log_q2.png");
 
-	c2->SaveAs("Jpsi_MCFit_2016.pdf");
-	c2->SaveAs("Jpsi_MCFit_2016.png");
+	c2->SaveAs("rare_MCFit_2016_q2.pdf");
+	c2->SaveAs("rare_MCFit_2016_q2.png");
 
-	c3->SaveAs("Jpsi_DataFit_2016.pdf");
-	c3->SaveAs("Jpsi_DataFit_2016.png");
+	c3->SaveAs("rare_DataFit_2016_q2.pdf");
+	c3->SaveAs("rare_DataFit_2016_q2.png");
 }
 
